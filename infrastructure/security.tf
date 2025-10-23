e44675665183c1d62117ea72ea2244d3b11e00c6
uallknowmatt <uallknowmatt@gmail.com>
2025-10-22 21:54:36 -0500

    feat: Allow inbound HTTP traffic to AKS LoadBalancer via NSG rule (port 80)

--- infrastructure/security.tf
+++ infrastructure/security.tf
@@ -9,6 +9,21 @@ resource "azurerm_network_security_group" "aks_nsg" {
   }
 }
 
+resource "azurerm_network_security_rule" "allow_http_inbound" {
+  name                        = "Allow-HTTP-Inbound"
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.aks_nsg.name
+  resource_group_name         = azurerm_resource_group.rg.name
+  description                 = "Allow inbound HTTP traffic to AKS LoadBalancer"
+}
+
 resource "azurerm_subnet_network_security_group_association" "aks_nsg_assoc" {
   subnet_id                 = azurerm_subnet.aks_subnet.id
   network_security_group_id = azurerm_network_security_group.aks_nsg.id
